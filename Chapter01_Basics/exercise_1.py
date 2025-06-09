4c701ffcb48bf90535d4d05dadc6976dfff2312e
OleksandraDee <oleksandra.dembitska@gmail.com>
2025-06-09 21:38:00 +0100

    first commit

---
+++ Chapter01_Basics/exercise_1.py
@@ -0,0 +1,12 @@
+"""
+Create a variable called `name` and assign your name to it, 
+for example: 'Danylo'.
+
+Create a variable called `age` and assign your age to it.
+"""
+
+name = 'Danylo'  
+age = 30  
+
+print(f"My name is {name} and I am {age} years old.")
+# Output: My name is Danylo and I am 30 years old.
